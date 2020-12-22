9cbaf676dcdd629132b9048d417ff45e407bdcd4
Massimiliano Giovagnoli <massimiliano.giovagnoli.1992@gmail.com>
2020-12-22 18:46:09 +0100

    feat(makefile/vim): automate vim plugin and requirements

---
+++ bin/install-ospackage.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PACKAGE="${1}"
+DISTRO="$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')"
+
+if [ "${DISTRO}" == "nixos" ]; then
+  nix-env -iA "nixos.${PACKAGE}"
+elif ["${DISTRO}" == "debian" ]; then
+  sudo apt-get update && \
+    sudo apt-get install -y "${PACKAGE}"
+elif ["${DISTRO}" == "arch" ]; then
+  sudo pacman -S "${PACKAGE}"
+fi
