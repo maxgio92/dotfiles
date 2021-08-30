9a42e38c6f1ebd49e395e51c87af82df8cad8b56
maxgio92 <massimiliano.giovagnoli.1992@gmail.com>
2021-08-30 18:44:33 +0200

    feat: add installation for ubuntu too

--- bin/install-ospackage.sh
+++ bin/install-ospackage.sh
@@ -5,9 +5,12 @@ DISTRO="$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')"
 
 if [ "${DISTRO}" == "nixos" ]; then
   nix-env -iA "nixos.${PACKAGE}"
-elif ["${DISTRO}" == "debian" ]; then
+elif [ "${DISTRO}" == "debian" ]; then
   sudo apt-get update && \
     sudo apt-get install -y "${PACKAGE}"
-elif ["${DISTRO}" == "arch" ]; then
+elif [ "${DISTRO}" == "ubuntu" ]; then
+  sudo apt update && \
+    sudo apt install -y "${PACKAGE}"
+elif [ "${DISTRO}" == "arch" ]; then
   sudo pacman -S "${PACKAGE}"
 fi
