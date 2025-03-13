57d5aa9c3a9b75d141d54f07ef3a21acd956af25
Massimiliano Giovagnoli <me@maxgio.it>
2025-03-13 10:58:51 +0100

    feat(nvim): add gruvbox colorscheme - my eyes thanks me
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,105 +1,41 @@
 local vim = vim
-local Plug = vim.fn['plug#']
+
+-- Color scheme
+vim.cmd [[set nu rnu]]
 
 -- Plugins
-vim.call('plug#begin')
+local Plug = vim.fn['plug#']
 
-Plug('neovim/nvim-lspconfig') -- LSP
-Plug('nvim-treesitter/nvim-treesitter') -- Code navigation
-Plug('ray-x/guihua.lua', {['do'] = 'cd lua/fzy && make' })
-Plug('ray-x/navigator.lua')
+vim.call('plug#begin')
+Plug('ray-x/go.nvim') -- Go
+Plug('hrsh7th/vim-vsnip') -- Snippet engine
 Plug('hrsh7th/cmp-nvim-lsp') -- Completion
 Plug('hrsh7th/cmp-buffer')
 Plug('hrsh7th/cmp-path')
 Plug('hrsh7th/cmp-cmdline')
 Plug('hrsh7th/nvim-cmp')
-Plug('hrsh7th/cmp-vsnip')
-Plug('hrsh7th/vim-vsnip')
-Plug('ray-x/go.nvim') -- Go
-Plug('mfussenegger/nvim-dap') -- DAP
-Plug('leoluz/nvim-dap-go') -- Go Debug Adapter (requires Delve debugger)
-Plug('nvim-neotest/nvim-nio')
-Plug('rcarriga/nvim-dap-ui')
-Plug('nvim-lua/plenary.nvim') -- Fuzzy finder
-Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
-Plug('BurntSushi/ripgrep')
-Plug('nvim-telescope/telescope-fzf-native.nvim', {['do'] = 'make' })
-Plug('ray-x/aurora') -- Theme
-
+Plug('hrsh7th/cmp-vsnip') -- Completion with snippet 
+Plug('ellisonleao/gruvbox.nvim') -- Colorscheme
 vim.call('plug#end')
 
--- Navigator
-require'navigator'.setup()
 
--- Set up nvim-cmp.
-local cmp = require'cmp'
+-- Language Servers
+local lspconfig = require("lspconfig")
 
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-  }, {
-    { name = 'buffer' },
-  })
+-- Terraform Language Server
+lspconfig.terraformls.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
 })
 
--- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
--- Set configuration for specific filetype.
---[[ cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'git' },
-  }, {
-    { name = 'buffer' },
-  })
-)
-require("cmp_git").setup() ]]-- 
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  }),
-  matching = { disallow_symbol_nonprefix_matching = false }
-})
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig')['gopls'].setup {
-  capabilities = capabilities
-}
+-- Go Language Server
+lspconfig.gopls.setup({})
 
 -- Go plugin setup
 require('go').setup()
-
 -- Run goimports and gofmt on save
 local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -110,40 +46,42 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   group = format_sync_grp,
 })
 
--- DAP
-local dap = require("dap")
-dap.adapters.delve = {
-  type = 'server',
-  port = '${port}',
-  executable = {
-    command = 'dlv',
-    args = {'dap', '-l', '127.0.0.1:${port}'},
-  }
-}
-require("dap-go").setup({
-    dap_configurations = {
-        {
-            type = "go",
-            name = "Debug (Build Flags & Arguments)",
-            request = "launch",
-            --program = "${file}",
-            program = "./${relativeFileDirname}",
-            args = require("dap-go").get_arguments,
-            buildFlags = require("dap-go").get_build_flags,
-        },
-    }
+-- Completion
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    -- REQUIRED
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+    end,
+  },
+  window = {},
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+  }, {
+    { name = 'buffer' },
+  })
 })
-require("dapui").setup()
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
--- Fuzzy finder
-require('telescope').setup()
-require('telescope').load_extension('fzf')
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- Color scheme
-vim.cmd [[colorscheme vim]]
-vim.cmd [[set nu rnu]]
+vim.cmd [[set mouse=]]
+
+--- Colorscheme
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
