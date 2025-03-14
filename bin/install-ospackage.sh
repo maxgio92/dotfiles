b05c5efcfce5393fc6f4d3f6e5bfe7d112080abe
Massimiliano Giovagnoli <me@maxgio.it>
2025-03-14 12:00:19 +0100

    chore(bin/install-ospackage.sh): add support for wolfi
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- bin/install-ospackage.sh
+++ bin/install-ospackage.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
+# Usage: install-ospackage.sh NAME
+# This is pretty rough.
 PACKAGE="${1}"
 DISTRO="$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')"
 
-if [ "${DISTRO}" == "nixos" ]; then
-  nix-env -iA "nixos.${PACKAGE}"
-elif [ "${DISTRO}" == "debian" ]; then
-  sudo apt-get update && \
-    sudo apt-get install -y "${PACKAGE}"
-elif [ "${DISTRO}" == "ubuntu" ]; then
-  sudo apt update && \
-    sudo apt install -y "${PACKAGE}"
-elif [ "${DISTRO}" == "arch" ]; then
-  sudo pacman -S "${PACKAGE}"
-fi
+case "${DISTRO}" in
+	nixos)
+		nix-env -iA "nixos.${PACKAGE}"
+		;;
+	debian)
+		sudo apt-get update && \
+			sudo apt-get install \
+				--no-install-recommends -y \
+			$PACKAGE
+		;;
+	ubuntu)
+		sudo apt-get update && \
+			sudo apt-get install \
+				--no-install-recommends -y \
+			$PACKAGE
+		;;
+	arch)
+		sudo pacman -S $PACKAGE
+		;;
+	wolfi)
+		sudo apk add $PACKAGE
+		;;
+esac
+
